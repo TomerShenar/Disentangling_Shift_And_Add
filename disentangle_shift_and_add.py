7386d41de50cf0812cb50393824f6b1b1cb889aa
Tomer Shenar <90908493+TomerShenar@users.noreply.github.com>
2023-09-11 16:12:43 +0200

    Update disentangle_shift_and_add.py

--- disentangle_shift_and_add.py
+++ disentangle_shift_and_add.py
@@ -1,13 +1,11 @@
 # shift-and-add & grid disentangling, by Tomer Shenar, with contributions from Matthias Fabry & Julia Bodensteiner
-# 21.11.2022, V1.0; feel free to contact at T.Shenar@uva.nl or tomer.shenar@gmail.com for questions/inquires
+# 11.09.2023, V2.0; feel free to contact at T.Shenar@uva.nl or tomer.shenar@gmail.com for questions/inquires
 # Algorithm and examples in Gonzales & Levato 2006, A&A, 448, 283; Shenar et al. 2021, A&A, 639, 6; Shenar et al. 2022, A&A, 665, 148
-# Current version only applicable for binaries. 
+# Current version only applicable for binaries and triples (no quadruples yet, although structure is there). 
 # Input: input file and observed spectra.
-# Output: chi2 map on K1,K2 plane (if requested) and separated spectra.
+# Output: chi2 map on K1,K2,K3 plane (if requested) and separated spectra.
 # See "input file" for more documentation
-# Coming up in upcoming versions: Higher-order multiples and nebular contamination. 
-
-
+# Coming up in upcoming versions: Quadruples and nebular contamination. 
 
 from Input_disentangle import *
 from Disentangling.disentangle_functions import * 
@@ -19,7 +17,7 @@ from Disentangling.disentangle_functions import *
 if ObsFormat=='TXT':
     PhaseFiles = ascii.read(ObsPath + 'ObsDat.txt')
     MJDs = PhaseFiles['MJD']
-    specnames = np.array([ObsPath + el for el in PhaseFiles['obsname']])
+    specnames = PhaseFiles['obsname']
 elif ObsFormat=='FITS':
     specnames = glob.glob(ObsPath+ '/*.fits')
     MJDs = np.array([])
@@ -37,8 +35,8 @@ if PLOTITR and PLOTCONV:
 if PLOTEXTREMES and PLOTFITS:
     print("ERROR: avoid having both PLOTEXTREMES and PLOTFITS true")
     sys.exit("Exit in disentangle_shift_and_add.py")
-if CompNum != 2:
-    print("ERROR: Can't handle more than two components with current version.")
+if CompNum > 3:
+    print("ERROR: Can't handle more than three components with current version.")
     sys.exit("Exit in disentangle_shift_and_add.py")
 
 # vector of light ratios l1, l2, l3, l4
@@ -75,7 +73,7 @@ for i, filepath in enumerate(specnames):
         SpecClean = np.copy(spec)
 # Can also re-normalise spectra... better avoid
     if Renormalise and GridDis:
-        SpecNorm = Normalise(np.copy(SpecClean, points=NormPoints))
+        SpecNorm = Normalise(np.copy(SpecClean), points=NormPoints)
     else:
         SpecNorm= np.copy(SpecClean)
     ObsSpecs.append(SpecNorm)
@@ -98,6 +96,13 @@ MsData = 2 * np.pi * phisData
 EsData =  Kepler(1., MsData, Orbital_Params['ecc'])
 eccfac = np.sqrt((1 + Orbital_Params['ecc']) / (1 - Orbital_Params['ecc']))
 nusdata = 2. * np.arctan(eccfac * np.tan(0.5 * EsData))
+
+#For outer tertiary:
+phisDataOut = (MJDs-Orbital_Params['T0Out'])/Orbital_Params['PeriodOut'] - ((MJDs-Orbital_Params['T0Out'])/Orbital_Params['PeriodOut']).astype(int)
+MsDataOut = 2 * np.pi * phisDataOut
+EsDataOut =  Kepler(1., MsDataOut, Orbital_Params['eccOut'])
+eccfacOut = np.sqrt((1 + Orbital_Params['eccOut']) / (1 - Orbital_Params['eccOut']))
+nusdataOut = 2. * np.arctan(eccfacOut * np.tan(0.5 * EsDataOut))
 
 
 
@@ -154,9 +159,9 @@ if DenseKArr[1] == 1:
 else:
     K2s = np.linspace(IniFacKArr[1]*Orbital_Params['K2'], FinFacKArr[1]*Orbital_Params['K2'], DenseKArr[1])
 if DenseKArr[2] == 1:
-    K3s = np.array([Orbital_Params['K3'] ])
+    KOuts = np.array([Orbital_Params['KOut'] ])
 else:
-    K3s = np.linspace(IniFacKArr[2]*Orbital_Params['K3'], FinFacKArr[2]*Orbital_Params['K3'], DenseKArr[2])
+    KOuts = np.linspace(IniFacKArr[2]*Orbital_Params['KOut'], FinFacKArr[2]*Orbital_Params['KOut'], DenseKArr[2])
 if DenseKArr[3] == 1:
     K4s = np.array([Orbital_Params['K4'] ])
 else:
@@ -164,9 +169,9 @@ else:
 
 
 
+
 if not os.path.exists('Output/'):
     os.mkdir('Output/')
-
 
 # Run main disentangling routine
 if GridDis:
@@ -174,55 +179,87 @@ if GridDis:
 # Compute RVs for comp1, comp2      
    vrads1, vrads2 = v1andv2(nusdata, Orbital_Params)     
    ScalingNeb = np.ones(len(vrads1))
-   kcount_extremeplot = np.argmin(np.abs(K1s - Velo_plot_usrK1_ext)) * len(K2s) + np.argmin(np.abs(K2s - Velo_plot_usrK2_ext))
-   kcount_usr = np.argmin(np.abs(K1s - Velo_plot_usrK1)) * len(K2s) + np.argmin(np.abs(K2s - Velo_plot_usrK2))
-   K1, K2 = Grid_disentangling2D(waveRanges, nusdata, CompArr[1], Orbital_Params, K1s, K2s,  ObsSpecs, weights, StrictNeg, PosLimCond, Poslimall, MJDs, phisData,  specnames, Rangestr, StarName, ScalingNeb, Ini='B', ShowItr=False,   InterKind=InterKind, itrnumlim = itrnumlim,   PLOTCONV=PLOTCONV, PLOTITR=PLOTITR,  PLOTEXTREMES=PLOTEXTREMES, PLOTFITS=PLOTFITS, kcount_extremeplot=kcount_extremeplot, linewidExt=linewidExt, CompNum=CompNum, ParbSize=ParbSize, N_Iteration_Plot=N_Iteration_Plot,  NebLines = NebLines, NebFac=1, kcount_usr=kcount_usr, ExtremesFigSize=ExtremesFigSize)
-   Orbital_Params['K1'] = K1
-   Orbital_Params['K2'] = K2
+   if CompNum==2:
+        kcount_extremeplot = np.argmin(np.abs(K1s - Velo_plot_usrK1_ext)) * len(K2s) + np.argmin(np.abs(K2s - Velo_plot_usrK2_ext))
+        kcount_usr = np.argmin(np.abs(K1s - Velo_plot_usrK1)) * len(K2s) + np.argmin(np.abs(K2s - Velo_plot_usrK2))
+        K1, K2 = Grid_disentangling2D(waveRanges, nusdata, CompArr[1], Orbital_Params, K1s, K2s,  ObsSpecs, weights, StrictNeg, PosLimCond, Poslimall, MJDs, phisData,  specnames, Rangestr, StarName, ScalingNeb, Ini='B', ShowItr=False,   InterKind=InterKind, itrnumlim = itrnumlim,   PLOTCONV=PLOTCONV, PLOTITR=PLOTITR,  PLOTEXTREMES=PLOTEXTREMES, PLOTFITS=PLOTFITS, kcount_extremeplot=kcount_extremeplot, linewidExt=linewidExt, ParbSize=ParbSize, N_Iteration_Plot=N_Iteration_Plot,  NebLines = NebLines, NebFac=1, kcount_usr=kcount_usr, ExtremesFigSize=ExtremesFigSize)
+        Orbital_Params['K1'] = K1
+        Orbital_Params['K2'] = K2        
+   elif CompNum==3:
+        kcount_extremeplot = np.argmin(np.abs(K1s - Velo_plot_usrK1_ext)) * len(K2s) * len(KOuts) + np.argmin(np.abs(K2s - Velo_plot_usrK2_ext)) * len(KOuts) + np.argmin(np.abs(KOuts - Velo_plot_usrK3_ext))
+        kcount_usr = np.argmin(np.abs(K1s - Velo_plot_usrK1)) * len(K2s) * len(KOuts) + np.argmin(np.abs(K2s - Velo_plot_usrK2)) * len(KOuts) + np.argmin(np.abs(KOuts - Velo_plot_usrK3))  
+        K1, K2, KOut = Grid_disentangling3D(waveRanges, nusdata, nusdataOut, CompArr[1], Orbital_Params, K1s, K2s,  KOuts, ObsSpecs, weights, StrictNeg, PosLimCond, Poslimall,  MJDs, phisData, phisDataOut, specnames, Rangestr, StarName, ScalingNeb, Ini='B', ShowItr=False,   InterKind=InterKind, itrnumlim = itrnumlim,   PLOTCONV=PLOTCONV, PLOTITR=PLOTITR,  PLOTEXTREMES=PLOTEXTREMES, PLOTFITS=PLOTFITS, kcount_extremeplot=kcount_extremeplot, linewidExt=linewidExt,  ParbSize=ParbSize, N_Iteration_Plot=N_Iteration_Plot,  NebLines = NebLines, NebFac=1, kcount_usr=kcount_usr, ExtremesFigSize=ExtremesFigSize)
+        Orbital_Params['K1'] = K1
+        Orbital_Params['K2'] = K2   
+        Orbital_Params['KOut'] = KOut
 else:  
     PLOTEXTREMES = False
     PLOTFITS = False
     K2 = Orbital_Params['K2']
     K1 = Orbital_Params['K1']
+    KOut = Orbital_Params['KOut']
     ScalingNeb = np.ones(len(phisData))
     #K2=2*K1
 
-print("K2 found:", K2)
+#print("K2 found:", K2)
 if K2 < 0:
     print('setting K2=2*K1')
     Orbital_Params['K2'] = 2*Orbital_Params['K1']
-print("disentangling...., K1, K2:", Orbital_Params['K1'], Orbital_Params['K2'])
-vrads1, vrads2 = v1andv2(nusdata, Orbital_Params)
-itrnumlim=NumItrFinal
-NebSpec = np.zeros(len(wavegridall))
-DisSpecVector, redchi2 = disentangle(np.zeros(len(wavegridall)), vrads1, vrads2,  wavegridall, ObsSpecs, weights, StrictNeg, PosLimCond, Poslimall,  nusdata, Orbital_Params, K1s, K2s, MJDs, phisData, specnames, Rangestr, StarName,  ScalingNeb, NebSpec, Resid=False, Reduce=True, ShowItr=True, Once=True,   InterKind=InterKind, itrnumlim=itrnumlim,   PLOTCONV=PLOTCONV, PLOTITR=PLOTITR,  PLOTEXTREMES=PLOTEXTREMES, PLOTFITS=PLOTFITS, kcount_extremeplot=0, linewidExt=linewidExt, CompNum=CompNum, N_Iteration_Plot=N_Iteration_Plot,  NebLines = NebLines, NebFac=1, ExtremesFigSize=ExtremesFigSize)
+if CompNum>2 and KOut < 0:
+    print('setting KOut=0')
+    Orbital_Params['KOut'] = 0.
+    
+if CompNum==2:
+    print("disentangling...., K1, K2:", Orbital_Params['K1'], Orbital_Params['K2'])
+    vrads1, vrads2 = v1andv2(nusdata, Orbital_Params)
+    itrnumlim=NumItrFinal
+    NebSpec = np.zeros(len(wavegridall))
+    DisSpecVector, redchi2 = disentangle(np.zeros(len(wavegridall)), vrads1, vrads2,  wavegridall, ObsSpecs, weights, StrictNeg, PosLimCond, Poslimall,  nusdata, Orbital_Params, K1s, K2s, MJDs, phisData, specnames, Rangestr, StarName,  ScalingNeb, NebSpec, Resid=False, Reduce=True, ShowItr=True, Once=True,   InterKind=InterKind, itrnumlim=itrnumlim,   PLOTCONV=PLOTCONV, PLOTITR=PLOTITR,  PLOTEXTREMES=PLOTEXTREMES, PLOTFITS=PLOTFITS, kcount_extremeplot=0, linewidExt=linewidExt, N_Iteration_Plot=N_Iteration_Plot,  NebLines = NebLines, NebFac=1, ExtremesFigSize=ExtremesFigSize)
+elif CompNum==3:
+    print("disentangling...., K1, K2, KOut:", Orbital_Params['K1'], Orbital_Params['K2'],  Orbital_Params['KOut'])
+    vrads1, vrads2 = v1andv2(nusdata, Orbital_Params)
+    vradsOut, vradsBin = v1andv2(nusdataOut, Orbital_Params, Config='Tertiary')
+    itrnumlim=NumItrFinal
+    NebSpec = np.zeros(len(wavegridall))
+    DisSpecVector, redchi2 = disentangle3Comp(np.zeros(len(wavegridall)), vrads1, vrads2, vradsOut, vradsBin,  wavegridall, ObsSpecs, weights, StrictNeg, PosLimCond, Poslimall, nusdata, nusdataOut, Orbital_Params, K1s, K2s, KOuts, MJDs, phisData, phisDataOut, specnames, Rangestr, StarName,  ScalingNeb, NebSpec,  Resid=False, Reduce=True, ShowItr=True, Once=True,   InterKind=InterKind, itrnumlim=itrnumlim,   PLOTCONV=PLOTCONV, PLOTITR=PLOTITR,  PLOTEXTREMES=PLOTEXTREMES, PLOTFITS=PLOTFITS, kcount_extremeplot=0, linewidExt=linewidExt,  N_Iteration_Plot=N_Iteration_Plot,  NebLines = NebLines, NebFac=1, ExtremesFigSize=ExtremesFigSize)      
+    
+    
 
-
-# These are the final, scaled spectra:
-A, B, NebSpec = DisSpecVector
-
+# These are the final, non-scaled spectra:
+if CompNum==2:
+    A, B, NebSpec = DisSpecVector
+elif CompNum==3:
+    A, B, C, NebSpec = DisSpecVector
+    
 A = (A-1)/ lguessVec[0] + 1.    
 B = (B-1)/lguessVec[1] + 1.
+plt.plot(wavegridall, A, label='dis A')
+plt.plot(wavegridall, B, label='dis B')
 if CompNum>=3:
     C = (C-1)/lguessVec[2] + 1.
+    plt.plot(wavegridall, C, label='dis C')
 if CompNum==4:
     D = (D-1)/lguessVec[3] + 1.
 
 
 
-plt.plot(wavegridall, A, label='dis A')
-plt.plot(wavegridall, B, label='dis B')
+
 if NebLines:
     plt.plot(wavegridall, NebSpec, label='dis Neb')
 
 
 
 
-np.savetxt('Output/ADIS_lguess2_K1K2=' + str(lguessVec[1]) + '_' + str(Orbital_Params['K1']) + '_' +  str(Orbital_Params['K2']) + '.txt', np.c_[wavegridall, A])
-np.savetxt('Output/BDIS_lguess2_K1K2=' + str(lguessVec[1]) + '_' + str(Orbital_Params['K1']) + '_' + str(Orbital_Params['K2']) + '.txt', np.c_[wavegridall, B])
+if CompNum==2:
+    np.savetxt('Output/ADIS_lguess2_K1K2=' + str(lguessVec[1]) + '_' + str(Orbital_Params['K1']) + '_' +  str(Orbital_Params['K2']) + '.txt', np.c_[wavegridall, A])
+    np.savetxt('Output/BDIS_lguess2_K1K2=' + str(lguessVec[1]) + '_' + str(Orbital_Params['K1']) + '_' + str(Orbital_Params['K2']) + '.txt', np.c_[wavegridall, B])
+elif CompNum==3:
+    np.savetxt('Output/ADIS_lguess2_lguess3_K1K2KOut=' + str(lguessVec[1]) + '_'  + str(lguessVec[2]) + '_' + str(Orbital_Params['K1']) + '_' +  str(Orbital_Params['K2']) +'_' +  str(Orbital_Params['KOut'])  +   '.txt', np.c_[wavegridall, A])
+    np.savetxt('Output/BDIS_lguess2_lguess3_K1K2KOut=' + str(lguessVec[1]) + '_'  + str(lguessVec[2]) + '_' + str(Orbital_Params['K1']) + '_' +  str(Orbital_Params['K2']) +'_' +  str(Orbital_Params['KOut'])  +   '.txt', np.c_[wavegridall, B])
+    np.savetxt('Output/CDIS_lguess2_lguess3_K1K2KOut=' + str(lguessVec[1]) + '_'  + str(lguessVec[2]) + '_' + str(Orbital_Params['K1']) + '_' +  str(Orbital_Params['K2']) +'_' +  str(Orbital_Params['KOut'])  +   '.txt', np.c_[wavegridall, C])
 if NebLines:
     np.savetxt('Output/NebDIS_lguess2_K1K2=' + str(lguessVec[1]) + '_' + str(Orbital_Params['K1']) + '_' + str(Orbital_Params['K2']) + '.txt', np.c_[wavegridall, NebSpec])
-
+    
 plt.legend()
 plt.show()
 
